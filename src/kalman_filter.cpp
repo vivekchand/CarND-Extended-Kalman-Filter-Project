42fff717f7922fee2ec98b2ab8bfa2bd2c707880
Vivek Chand <vivekchand19@gmail.com>
2017-10-12 00:19:06 +0530

    code cleanup

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -9,51 +9,59 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-  x_ = F_ * x_ ; // There is no external motion, so, we do not have to add "+u"
-  MatrixXd Ft = F_.transpose();
-  P_ = F_ * P_ * Ft + Q_;
+    x_ = F_ * x_; // There is no external motion, so, we do not have to add "+u"
+    MatrixXd Ft = F_.transpose();
+    P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  // Kalman filter update step. Equations from the lectures
-  VectorXd y = z - H_ * x_; // error calculation
-  KF(y);
+    VectorXd z_pred = H_ * x_;
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-    * update the state by using Extended Kalman Filter equations
-  */
-  // Recalculate x object state to rho, theta, rho_dot coordinates
-  double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
-  double theta = atan2(x_(1), x_(0));
-  double rho_dot = (x_(0)*x_(2) + x_(1)*x_(3)) / rho;
-  VectorXd h = VectorXd(3); // h(x_)
-  h << rho, theta, rho_dot;
+    double px = x_[0];
+    double py = x_[1];
+    double vx = x_[2];
+    double vy = x_[3];
 
-  VectorXd y = z - h;
-  // Calculations are essentially the same to the Update function
-  KF(y);
-}
+    double rho = sqrt(px * px + py * py);
+    double phi = atan2(py, px);
+    double rho_dot = (px * vx + py * vy) / rho;
 
-// Universal update Kalman Filter step. Equations from the lectures
-void KalmanFilter::KF(const VectorXd &y){
-  MatrixXd Ht = H_.transpose();
-  MatrixXd S = H_ * P_ * Ht + R_;
-  MatrixXd Si = S.inverse();
-  MatrixXd K =  P_ * Ht * Si;
-  // New state
-  x_ = x_ + (K * y);
-  int x_size = x_.size();
-  MatrixXd I = MatrixXd::Identity(x_size, x_size);
-  P_ = (I - K * H_) * P_;
+    VectorXd z_pred(3);
+    z_pred << rho, phi, rho_dot;
+
+    VectorXd y = z - z_pred;
+    MatrixXd Ht = H_.transpose();
+    MatrixXd S = H_ * P_ * Ht + R_;
+    MatrixXd Si = S.inverse();
+    MatrixXd PHt = P_ * Ht;
+    MatrixXd K = PHt * Si;
+
+    //new estimate
+    x_ = x_ + (K * y);
+    long x_size = x_.size();
+    MatrixXd I = MatrixXd::Identity(x_size, x_size);
+    P_ = (I - K * H_) * P_;
 }
